25c01eb47a42701a9c07d74decfb27f56130cc55
duck0249 <donggeunlee@Donggeuns-MacBook-Air.local>
2024-10-29 08:50:56 +0700

    Final code

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from entities.Mario import Mario
 
 windowSize = 640, 480
 
-
+# status
 def main():
     pygame.mixer.pre_init(44100, -16, 2, 4096) # Set audio optons
     pygame.init() # Initializes the library
